c4d765b2ba6e55dc8c9633181ec38c23e6780a2a
Sebastian Wiesner <lunaryorn@googlemail.com>
2011-02-05 18:46:04 +0100

    0.5 branch is 0.5.2 now

--- synaptiks/__init__.py
+++ synaptiks/__init__.py
@@ -71,7 +71,7 @@ from __future__ import (print_function, division, unicode_literals,
                         absolute_import)
 
 
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 
 WEBSITE_URL = 'http://synaptiks.lunaryorn.de'
 ISSUE_TRACKER_URL = 'https://github.com/lunaryorn/synaptiks/issues'
