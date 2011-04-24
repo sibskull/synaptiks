e7d00c05ca7abefdb8b5746b22e4a3be6d6d0824
Sebastian Wiesner <lunaryorn@googlemail.com>
2011-04-24 10:58:09 +0200

    Also fall back to ~/.config, if $XDG_CONFIG_HOME is empty (as required by spec)

--- synaptiks/config.py
+++ synaptiks/config.py
@@ -121,8 +121,9 @@ def get_configuration_directory():
 
     .. _`XDG base directory specification`: http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
     """
-    xdg_config_home = os.environ.get('XDG_CONFIG_HOME', os.path.expandvars(
-        os.path.join('$HOME', '.config')))
+    xdg_config_home = os.environ.get('XDG_CONFIG_HOME')
+    if not xdg_config_home:
+        xdg_config_home = os.path.expandvars(os.path.join('$HOME', '.config'))
     return ensure_directory(os.path.join(xdg_config_home, 'synaptiks'))
 
 
