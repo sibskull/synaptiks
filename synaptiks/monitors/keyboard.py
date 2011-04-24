sibskull/synaptiks

commit 84b2fb1255263ce20384e42fdd7380c81b3b494f

Added synaptiks.xlib module with a general display class
This improves and unifies the Display API used within synaptiks, and makes
direct access to the Xlib bindings largely superfluous.

synaptiks/monitors/keyboard.py
@@ -43,7 +43,7 @@ from itertools import izip
 from PyQt4.QtCore import QObject, QTimer, QTime, QThread, pyqtSignal
 from PyQt4.QtGui import QApplication
 
-from synaptiks.qx11 import QX11Display
+from synaptiks.xlib import Display
 from synaptiks._bindings import xlib
 from synaptiks._bindings import xrecord
 from synaptiks._bindings.util import scoped_pointer
@@ -214,7 +214,7 @@ class EventRecorder(QThread):
         if not self.isRunning():
             return
         self._started.wait()
-        xrecord.disable_context(QX11Display(), self._context)
+        xrecord.disable_context(Display.from_qt(), self._context)
         # immediately process the end of data event.  This allows us to wait
         # for this thread to terminate in the next line, thus making this
         # method synchronous.
@@ -254,7 +254,7 @@ class RecordingKeyboardMonitor(AbstractKeyboardMonitor):
         self._recorder.started.connect(self.started)
         self._recorder.finished.connect(self.stopped)
         # a set of all known modifier keycodes
-        modifier_mapping = xlib.get_modifier_mapping(QX11Display())
+        modifier_mapping = xlib.get_modifier_mapping(Display.from_qt())
         self._modifiers = frozenset(keycode for modifiers in modifier_mapping
                                     for keycode in modifiers if keycode != 0)
         # a set holding all pressed, but not yet released modifier keys
@@ -395,7 +395,7 @@ class PollingKeyboardMonitor(AbstractKeyboardMonitor):
     def _setup_mask(self):
         mask = array(b'B', b'\xff' * 32)
         if self._keys_to_ignore >= self.IGNORE_MODIFIER_KEYS:
-            modifier_mappings = xlib.get_modifier_mapping(QX11Display())
+            modifier_mappings = xlib.get_modifier_mapping(Display.from_qt())
             for modifier_keys in modifier_mappings:
                 for keycode in modifier_keys:
                     mask[keycode // 8] &= ~(1 << (keycode % 8))
@@ -405,7 +405,7 @@ class PollingKeyboardMonitor(AbstractKeyboardMonitor):
     def keyboard_active(self):
         is_active = False
 
-        _, raw_keymap = xlib.query_keymap(QX11Display())
+        _, raw_keymap = xlib.query_keymap(Display.from_qt())
         keymap = array(b'B', raw_keymap)
 
         is_active = keymap != self._old_keymap
@@ -457,7 +457,7 @@ def create_keyboard_monitor(parent=None):
     Return an implementation of :class:`AbstractKeyboardMonitor`.
     """
     if xrecord:
-        success, _ = xrecord.query_version(QX11Display())
+        success, _ = xrecord.query_version(Display.from_qt())
         if success:
             return RecordingKeyboardMonitor(parent)
     return PollingKeyboardMonitor(parent)

scripts/try_touchpad_config_widget.py
@@ -34,7 +34,7 @@ from PyKDE4.kdecore import KCmdLineArgs, KAboutData, ki18n
 from PyKDE4.kdeui import KApplication, KMainWindow
 
 import synaptiks
-from synaptiks.qx11 import QX11Display
+from synaptiks.xlib import Display
 from synaptiks.touchpad import Touchpad
 from synaptiks.config import TouchpadConfiguration
 from synaptiks.kde.widgets.touchpad import TouchpadConfigurationWidget
@@ -58,7 +58,7 @@ def main():
     KCmdLineArgs.init(sys.argv, about)
     app = KApplication()
     window = KMainWindow()
-    touchpad = Touchpad.find_first(QX11Display())
+    touchpad = Touchpad.find_first(Display.from_qt())
     config = TouchpadConfiguration(touchpad)
     config_widget = TouchpadConfigurationWidget(config)
     config_widget.configurationChanged.connect(

synaptiks/_bindings/__init__.py
@@ -35,8 +35,8 @@
        All contents of this package are internal, and should not be used by 3rd
        parties.  Even inside the **synaptiks** sources, these bindings should
        only be used directly when necessary, in most cases, the higher level
-       API provided by :mod:`synaptiks.xinput` and :mod:`synaptiks.touchpad` is
-       the right choice.
+       API provided by :mod:`synaptiks.xlib`, :mod:`synaptiks.xinput` and
+       :mod:`synaptiks.touchpad` is the right choice.
 
     .. moduleauthor::  Sebastian Wiesner  <lunaryorn@googlemail.com>
 """

synaptiks/_bindings/xlib.py
@@ -41,7 +41,6 @@ from itertools import islice, izip
 from ctypes import (Structure, POINTER, string_at, create_string_buffer,
                     c_uint32, c_int, c_void_p, c_char_p, c_char, c_ubyte,
                     c_ulong)
-from contextlib import contextmanager
 
 from synaptiks._bindings.util import load_library, scoped_pointer
 
@@ -132,56 +131,8 @@ SIGNATURES = dict(
 libX11 = load_library('X11', SIGNATURES)
 
 
-class DisplayError(EnvironmentError):
-    """
-    Raised if a display could not be opened.
-    """
-    pass
-
-
-def open_display(name=None):
-    """
-    Open the display with the given ``name``.
-
-    ``name`` is a byte string with the display name, or ``None``, in which case
-    the display name is lookup up in the `$DISPLAY` environment variable.
-
-    Return a :class:`Display_p` pointer to the display.  Raise
-    :exc:`DisplayError`, if the display could not be opened.
-    """
-    display = libX11.XOpenDisplay(name)
-    if not display:
-        raise DisplayError()
-    return display
-
-
-def close_display(display):
-    """
-    Close the given ``display`` (a :class:`Display_p` object).
-    """
-    if display:
-        libX11.XCloseDisplay(display)
-
-
-@contextmanager
-def display(name=None):
-    """
-    Connect to a X11 display using :func:`open_display`.
-
-    The connection is wrapped in a context manager, which closes the display
-    automatically, once the context is left::
-
-       with xlib.display() as display:
-           # work with the display here
-
-    ``name`` is passed to :func:`open_display`.
-
-    Return a :class:`Display_p` object with the new connection to the X11
-    display.  Raise :exc:`DisplayError`, if the display could not be opened.
-    """
-    display = open_display(name)
-    yield display
-    close_display(display)
+open_display = libX11.XOpenDisplay
+close_display = libX11.XCloseDisplay
 
 
 # add libX11 functions to top-level namespace under pythonic names

synaptiks/config.py
@@ -413,8 +413,8 @@ def main():
     from argparse import ArgumentParser
 
     from synaptiks import __version__
+    from synaptiks.xlib import Display, DisplayError
     from synaptiks.touchpad import Touchpad, NoTouchpadError
-    from synaptiks._bindings import xlib
 
     parser = ArgumentParser(
         description='synaptiks touchpad configuration utility',
@@ -452,7 +452,7 @@ distributed under the terms of the BSD License""")
     args = parser.parse_args()
 
     try:
-        with xlib.display() as display:
+        with Display.from_name() as display:
             touchpad = Touchpad.find_first(display)
 
             if args.action == 'init':
@@ -463,7 +463,7 @@ distributed under the terms of the BSD License""")
             if args.action == 'save':
                 current_config = TouchpadConfiguration(touchpad)
                 current_config.save(filename=args.filename)
-    except xlib.DisplayError:
+    except DisplayError:
         parser.error('could not connect to X11 display')
     except NoTouchpadError:
         parser.error('no touchpad found')

synaptiks/kde/trayapplication.py
@@ -47,7 +47,7 @@ from PyKDE4.kdeui import (KUniqueApplication, KStatusNotifierItem,
                           KHelpMenu, KIcon, KIconLoader,
                           KNotification, KConfigSkeleton)
 
-from synaptiks.qx11 import QX11Display
+from synaptiks.xlib import Display
 from synaptiks.touchpad import Touchpad
 from synaptiks.management import TouchpadManager
 from synaptiks.config import TouchpadConfiguration, ManagerConfiguration
@@ -136,7 +136,7 @@ class SynaptiksNotifierItem(KStatusNotifierItem):
         self._config = SynaptiksTrayConfiguration(self)
 
         try:
-            self.touchpad = Touchpad.find_first(QX11Display())
+            self.touchpad = Touchpad.find_first(Display.from_qt())
         except Exception as error:
             # show an error message
             from synaptiks.kde.error import get_localized_error_message

synaptiks/kde/widgets/kcm.py
@@ -150,11 +150,11 @@ def make_kcm_widget(component_data, parent=None):
     Otherwise a :class:`TouchpadErrorKCM` is returned, which gives the user a
     description of the error and its cause.
     """
-    from synaptiks.qx11 import QX11Display
+    from synaptiks.xlib import Display
     from synaptiks.touchpad import Touchpad
     from synaptiks.config import TouchpadConfiguration
     try:
-        touchpad = Touchpad.find_first(QX11Display())
+        touchpad = Touchpad.find_first(Display.from_qt())
         config = TouchpadConfiguration(touchpad)
         return TouchpadConfigurationKCM(config, component_data, parent)
     except Exception as error:

synaptiks/touchpad.py
@@ -38,8 +38,8 @@
     :meth:`Touchpad.find_first()` to get a :class:`Touchpad` object for this
     touchpad:
 
-    >>> from synaptiks.qx11 import QX11Display
-    >>> touchpad = Touchpad.find_first(QX11Display())
+    >>> from synaptiks.xlib import Display
+    >>> touchpad = Touchpad.find_first(Display.from_qt())
     >>> touchpad.name
     u'AlpsPS/2 ALPS GlidePoint'
 
@@ -152,9 +152,7 @@ class Touchpad(InputDevice):
         """
         Find all touchpad devices registered on the given ``display``.
 
-        ``display`` is X11 display object (see
-        :class:`synaptiks.qx11.QX11Display` or
-        :func:`synaptiks._bindings.xlib.display`).
+        ``display`` is a :class:`~synaptiks.xlib.Display` object.
 
         Return an iterator over all :class:`Touchpad` objects present on this
         system.
@@ -170,9 +168,7 @@ class Touchpad(InputDevice):
         Find the first usable touchpad device on this system and return it as
         :class:`Touchpad` object.
 
-        ``display`` is X11 display object (see
-        :class:`synaptiks.qx11.QX11Display` or
-        :func:`synaptiks._bindings.xlib.display`).
+        ``display`` is a :class:`~synaptiks.xlib.Display` object.
 
         Raise :exc:`NoTouchpadError`, if no touchpad was found.  Raise
         :exc:`synaptiks.xinput.XInputVersionError`, if the XInput version isn't

commit dd36ecc25c9ecdd09b51e11c1b3604a967f349d8

Fixed regression from 84b2fb1

synaptiks/monitors/keyboard.py
@@ -194,7 +194,7 @@ class EventRecorder(QThread):
 
     def run(self):
         # create a special display connection for recording
-        with xlib.display() as recording_display:
+        with Display.from_name() as recording_display:
             # record all key presses and releases, as these events indicate
             # keyboard activity
             key_events = (xlib.KEY_PRESS, xlib.KEY_RELEASE)