443746e2836b70142a87a9e34c1c4cb941083321
Sebastian Wiesner <lunaryorn@googlemail.com>
2011-06-30 18:14:45 +0200

    synaptiks can be considered stable now

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     platforms='X11',
     license='BSD',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: X11 Applications :: KDE',
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: BSD License',
